38fa974e62f95a8c3677a0aa0136ceff424d17e0
BrunoPereira-ctrl <brunopereiradosreis369@gmail.com.br>
2023-02-12 11:28:41 -0300

    database H2 seeding

---
+++ src/main/java/com/bruno/dscatalog/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.bruno.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bruno.dscatalog.model.Category;
+import com.bruno.dscatalog.repositories.CategoryRepository;
+
+
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+
+}
